6aada5193fc5ba795557c43f864e204832435448
Michael Gasperl <michael@gasperl.at>
2025-11-28 23:43:35 +0100

    Update tl_module.php
    Modernize tl_module DCA for filter_event_tags:
    	•	switched to Doctrine array SQL definition using MySQLPlatform::LENGTH_LIMIT_BLOB
    	•	ensured Contao 4.13 / 5.3 compatibility
    	•	cleaned palette injection and improved field definition consistency

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,12 +1,16 @@
 <?php
 
-// Tag-Filter-Feld an Eventlisten-Module anhängen
-if (isset($GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'])) {
-    $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] .= ';{tags_legend},filter_event_tags';
-}
+use Doctrine\DBAL\Platforms\MySQLPlatform;
 
+/**
+ * --------------------------------------------------------------------------
+ * Feld: filter_event_tags
+ * --------------------------------------------------------------------------
+ * Wird im Frontend-Modul „Eventliste (mit Tag-Filter)“ verwendet.
+ * Speicherung als BLOB, kompatibel mit Contao 4.13 und 5.3.
+ */
 $GLOBALS['TL_DCA']['tl_module']['fields']['filter_event_tags'] = [
-    'label'            => ['Tags filtern', 'Es werden nur Events mit diesen Tags angezeigt'],
+    'label'            => ['Tags filtern', 'Es werden nur Events angezeigt, die mindestens einen dieser Tags besitzen.'],
     'exclude'          => true,
     'inputType'        => 'select',
     'options_callback' => ['Mandrael\\EventTagsBundle\\TagsHelper', 'getTags'],
@@ -15,5 +19,22 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['filter_event_tags'] = [
         'chosen'   => true,
         'tl_class' => 'clr w50',
     ],
-    'sql'              => "blob NULL",
+    'sql'              => [
+        'type'    => 'blob',
+        'length'  => MySQLPlatform::LENGTH_LIMIT_BLOB,
+        'notnull' => false,
+    ],
 ];
+
+/**
+ * --------------------------------------------------------------------------
+ * Integration des Feldes in das FE-Modul "eventlist_tags"
+ * --------------------------------------------------------------------------
+ * Das Modul wird im config.php registriert und erhält hier die Felder.
+ */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist_tags']
+    = str_replace(
+        'cal_calendar', 
+        'cal_calendar,filter_event_tags',
+        $GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist'] ?? 'cal_calendar,filter_event_tags'
+    );
