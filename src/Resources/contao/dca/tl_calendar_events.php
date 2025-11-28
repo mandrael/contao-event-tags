02a0c1f2db580ab2a61f8663a5097614784431f1
Michael Gasperl <michael@gasperl.at>
2025-11-28 22:08:48 +0100

    Update tl_calendar_events.php
    Fix: Paletten-Existenzprüfung in tl_calendar_events hinzugefügt, um PaletteNotFoundException zu verhindern.

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -2,28 +2,30 @@
 
 use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-// 1. Feld-Definition (wie gehabt)
+// 1. Feld-Definition
 $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['event_tags'] = [
     'label'            => ['Tags', 'Tags für dieses Event auswählen.'],
     'exclude'          => true,
     'inputType'        => 'select',
-    // Hier binden wir den Helper ein (Service-Schreibweise oder statisch, je nachdem was du gewählt hast)
-    // Wenn du services.yaml nutzt: [Mandrael\EventTagsBundle\TagsHelper::class, 'getTags']
-    // Wenn du es klassisch hast: ['Mandrael\EventTagsBundle\TagsHelper', 'getTags']
     'options_callback' => ['Mandrael\EventTagsBundle\TagsHelper', 'getTags'],
     'eval'             => [
-        'multiple' => true, // Mehrfachauswahl
-        'chosen'   => true, // Aktiviert die Suche ("Schreiben und bestätigen")
+        'multiple' => true,
+        'chosen'   => true,
         'tl_class' => 'clr w50',
     ],
     'sql'              => "blob NULL",
 ];
 
-// 2. Positionierung: Feld 'event_tags' NACH 'author' einfügen
-// Wir nutzen den PaletteManipulator, das ist der sauberste Weg für Contao 4.13 & 5.3
-PaletteManipulator::create()
-    ->addField('event_tags', 'author', PaletteManipulator::POSITION_AFTER)
-    ->applyToPalette('default', 'tl_calendar_events')
-    ->applyToPalette('internal', 'tl_calendar_events')
-    ->applyToPalette('article', 'tl_calendar_events')
-    ->applyToPalette('external', 'tl_calendar_events');
+$manipulator = PaletteManipulator::create()
+    ->addField('event_tags', 'author', PaletteManipulator::POSITION_AFTER);
+
+// Liste der Paletten, die wir bearbeiten wollen
+$targetPalettes = ['default', 'internal', 'article', 'external'];
+
+// Wir gehen die Liste durch und wenden den Manipulator NUR an, 
+// wenn die Palette auch wirklich existiert.
+foreach ($targetPalettes as $paletteName) {
+    if (isset($GLOBALS['TL_DCA']['tl_calendar_events']['palettes'][$paletteName])) {
+        $manipulator->applyToPalette($paletteName, 'tl_calendar_events');
+    }
+}
