d8d83897477d7640b18016667d4b439bb85a634e
Michael Gasperl <michael@gasperl.at>
2025-11-28 23:21:03 +0100

    Update Plugin.php
    Fix: Ladereihenfolge des Plugins

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,6 +3,7 @@
 namespace Mandrael\EventTagsBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\CalendarBundle\ContaoCalendarBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
@@ -10,14 +11,14 @@ use Mandrael\EventTagsBundle\MandraelEventTagsBundle;
 
 class Plugin implements BundlePluginInterface
 {
-    /**
-     * {@inheritdoc}
-     */
     public function getBundles(ParserInterface $parser)
     {
         return [
             BundleConfig::create(MandraelEventTagsBundle::class)
-                ->setLoadAfter([ContaoCoreBundle::class])
+                ->setLoadAfter([
+                    ContaoCoreBundle::class,
+                    ContaoCalendarBundle::class,
+                ]),
         ];
     }
 }
