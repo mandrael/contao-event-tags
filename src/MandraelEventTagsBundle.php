4dce1f2da4cfd0d2aee7d023480466303111ea71
Michael Gasperl <michael@gasperl.at>
2025-11-28 21:46:10 +0100

    Update MandraelEventTagsBundle.php
    load services.yaml

--- src/MandraelEventTagsBundle.php
+++ src/MandraelEventTagsBundle.php
@@ -3,7 +3,21 @@
 namespace Mandrael\EventTagsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class MandraelEventTagsBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        // Hier laden wir die services.yaml, damit Dependency Injection funktioniert
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/Resources/config')
+        );
+        $loader->load('services.yaml');
+    }
 }
