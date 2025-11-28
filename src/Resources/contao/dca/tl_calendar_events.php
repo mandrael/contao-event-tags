001043976d3c4123961a0a8fc0b774aa5955f7d9
Michael Gasperl <michael@gasperl.at>
2025-11-28 21:39:15 +0100

    Update tl_calendar_events.php
    Position des Tag-Felds nach dem Feld Autor

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -1,24 +1,29 @@
 <?php
 
-// Legend hinzufügen, falls noch nicht vorhanden
-foreach ($GLOBALS['TL_DCA']['tl_calendar_events']['palettes'] as $paletteKey => $paletteDef) {
-    if (!is_string($paletteDef)) {
-        continue;
-    }
-    if (strpos($paletteDef, '{tags_legend') === false) {
-        $GLOBALS['TL_DCA']['tl_calendar_events']['palettes'][$paletteKey] .= ';{tags_legend},event_tags';
-    }
-}
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
+// 1. Feld-Definition (wie gehabt)
 $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['event_tags'] = [
-    'label'            => ['Tags', 'Tags für dieses Event'],
+    'label'            => ['Tags', 'Tags für dieses Event auswählen.'],
     'exclude'          => true,
     'inputType'        => 'select',
-    'options_callback' => ['Mandrael\\EventTagsBundle\\TagsHelper', 'getTags'],
+    // Hier binden wir den Helper ein (Service-Schreibweise oder statisch, je nachdem was du gewählt hast)
+    // Wenn du services.yaml nutzt: [Mandrael\EventTagsBundle\TagsHelper::class, 'getTags']
+    // Wenn du es klassisch hast: ['Mandrael\EventTagsBundle\TagsHelper', 'getTags']
+    'options_callback' => ['Mandrael\EventTagsBundle\TagsHelper', 'getTags'],
     'eval'             => [
-        'multiple' => true,
-        'chosen'   => true,
+        'multiple' => true, // Mehrfachauswahl
+        'chosen'   => true, // Aktiviert die Suche ("Schreiben und bestätigen")
         'tl_class' => 'clr w50',
     ],
     'sql'              => "blob NULL",
 ];
+
+// 2. Positionierung: Feld 'event_tags' NACH 'author' einfügen
+// Wir nutzen den PaletteManipulator, das ist der sauberste Weg für Contao 4.13 & 5.3
+PaletteManipulator::create()
+    ->addField('event_tags', 'author', PaletteManipulator::POSITION_AFTER)
+    ->applyToPalette('default', 'tl_calendar_events')
+    ->applyToPalette('internal', 'tl_calendar_events')
+    ->applyToPalette('article', 'tl_calendar_events')
+    ->applyToPalette('external', 'tl_calendar_events');
