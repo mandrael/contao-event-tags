f468a1f6c2ed14204a52947b30b0920a34a15a9c
Michael Gasperl <michael@gasperl.at>
2025-11-29 01:08:46 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,36 +1,36 @@
 <?php
 
-use Doctrine\DBAL\Platforms\MySQLPlatform;
-
 /**
  * Palette für das eigene Modul "eventlist_tags"
+ * * ÄNDERUNG: 'filter_event_tags' wurde direkt hinter 'type' geschoben.
  */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist_tags'] =
-    '{title_legend},name,headline,type;
-     {config_legend},cal_calendar,filter_event_tags;
-     {redirect_legend},jumpTo;
-     {template_legend:hide},customTpl;
-     {protected_legend:hide},protected;
-     {expert_legend:hide},guests,cssID;
-     {invisible_legend:hide},invisible,start,stop';
-
+$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist_tags'] = 
+    '{title_legend},name,headline,type,filter_event_tags;' . // Hier steht es jetzt direkt nach dem Typ
+    '{config_legend},cal_calendar;' . 
+    '{redirect_legend},jumpTo;' . 
+    '{template_legend:hide},customTpl;' . 
+    '{protected_legend:hide},protected;' . 
+    '{expert_legend:hide},guests,cssID;' . 
+    '{invisible_legend:hide},invisible,start,stop';
 
 /**
  * Feld: filter_event_tags
  */
 $GLOBALS['TL_DCA']['tl_module']['fields']['filter_event_tags'] = [
-    'label'            => ['Nach Tags filtern', 'Es werden nur Events mit diesen Tags angezeigt'],
+    'label'            => ['Nach Tags filtern', 'Es werden nur Events mit diesen Tags angezeigt.'],
     'exclude'          => true,
     'inputType'        => 'select',
-    'options_callback' => ['Mandrael\EventTagsBundle\Helper\TagsHelper', 'getTags'],
+    // Namespace angepasst auf deine Struktur (Helper Ordner)
+    'options_callback' => [Mandrael\EventTagsBundle\Helper\TagsHelper::class, 'getTags'],
     'eval'             => [
         'multiple' => true,
         'chosen'   => true,
         'tl_class' => 'clr', // volle Breite
     ],
+    // SQL-Definition (Array-Schreibweise für beste Kompatibilität)
     'sql'              => [
         'type'    => 'blob',
-        'length'  => MySQLPlatform::LENGTH_LIMIT_BLOB,
+        'length'  => 65535,
         'notnull' => false,
     ],
 ];
