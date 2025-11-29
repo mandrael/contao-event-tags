68c5f4988214b805fb288e62a2d2c4ad491b8600
Michael Gasperl <michael@gasperl.at>
2025-11-29 01:18:45 +0100

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -1,36 +1,31 @@
 <?php
 
-/**
- * Palette für das eigene Modul "eventlist_tags"
- * * ÄNDERUNG: 'filter_event_tags' wurde direkt hinter 'type' geschoben.
- */
-$GLOBALS['TL_DCA']['tl_module']['palettes']['eventlist_tags'] = 
-    '{title_legend},name,headline,type,filter_event_tags;' . // Hier steht es jetzt direkt nach dem Typ
-    '{config_legend},cal_calendar;' . 
-    '{redirect_legend},jumpTo;' . 
-    '{template_legend:hide},customTpl;' . 
-    '{protected_legend:hide},protected;' . 
-    '{expert_legend:hide},guests,cssID;' . 
-    '{invisible_legend:hide},invisible,start,stop';
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
 /**
- * Feld: filter_event_tags
+ * Feld-Definition (wie gehabt)
  */
 $GLOBALS['TL_DCA']['tl_module']['fields']['filter_event_tags'] = [
-    'label'            => ['Nach Tags filtern', 'Es werden nur Events mit diesen Tags angezeigt.'],
+    'label'            => ['Nach Tags filtern', 'Nur Events mit diesen Tags anzeigen.'],
     'exclude'          => true,
     'inputType'        => 'select',
-    // Namespace angepasst auf deine Struktur (Helper Ordner)
     'options_callback' => [Mandrael\EventTagsBundle\Helper\TagsHelper::class, 'getTags'],
     'eval'             => [
         'multiple' => true,
         'chosen'   => true,
-        'tl_class' => 'clr', // volle Breite
+        'tl_class' => 'clr',
     ],
-    // SQL-Definition (Array-Schreibweise für beste Kompatibilität)
     'sql'              => [
         'type'    => 'blob',
         'length'  => 65535,
         'notnull' => false,
     ],
 ];
+
+/**
+ * Positionierung: Wir hängen es in die STANDARD 'eventlist'
+ * Direkt nach der Kalender-Auswahl.
+ */
+PaletteManipulator::create()
+    ->addField('filter_event_tags', 'cal_calendar', PaletteManipulator::POSITION_AFTER)
+    ->applyToPalette('eventlist', 'tl_module');
