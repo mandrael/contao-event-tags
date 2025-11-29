106b1f9146ee538c597e26936b1e58553ab97c0e
Michael Gasperl <michael@gasperl.at>
2025-11-29 01:18:29 +0100

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,17 +1,7 @@
 <?php
 
-/**
- * ---------------------------------------------------------------
- * Backend-Module Registrierung
- * ---------------------------------------------------------------
- */
+// Backend-Modul (Inhalte -> Event-Tags)
 $GLOBALS['BE_MOD']['content']['event_tags'] = [
     'tables' => ['tl_event_tags'],
+    'icon'   => 'bundles/mandraeleventtags/icon.svg',
 ];
-
-/**
- * ---------------------------------------------------------------
- * Frontend-Module Registrierung
- * ---------------------------------------------------------------
- */
-$GLOBALS['FE_MOD']['events']['eventlist_tags'] = 'Mandrael\EventTagsBundle\Module\ModuleEventlistTags';
